c6ce799bb9b3ffe92b361e3e5f39206ad2a17b25
josuesan <josueguitar2@gmail.com>
2020-06-28 17:57:44 -0400

    first commit

---
+++ docs/precache-manifest.59b9a221db3bc7be6646fa84fd71e2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345fe94000e8503b7f726bc9c5e9fe96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d620f4ddb03d4d71dd",
+    "url": "/static/css/main.3d6669e5.chunk.css"
+  },
+  {
+    "revision": "b9f0338c6957298b4db5",
+    "url": "/static/js/2.8c692d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c692d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d620f4ddb03d4d71dd",
+    "url": "/static/js/main.ca10505e.chunk.js"
+  },
+  {
+    "revision": "603658e17899ebee4bd8",
+    "url": "/static/js/runtime-main.39ca27c7.js"
+  }
+]);
